3c1241b98a91a31a701a3e403e09c2e45dba71ec
alessandraciccone <alessandraciccone@icloud.com>
2025-12-17 15:52:58 +0100

    update pom

--- demo/src/main/java/aciccone/demo/GestioneRappIntApplication.java
+++ demo/src/main/java/aciccone/demo/GestioneRappIntApplication.java
@@ -1,13 +1,11 @@
 package aciccone.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestioneRappIntApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestioneRappIntApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneRappIntApplication.class, args);
+    }
 }
